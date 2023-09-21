cc27295e3e05fa34aafff8e69e7323c1653d4db5
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-21 15:42:24 +0200

    add bonus 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script>
+import appTitle from './components/AppTitle.vue'
 export default {
+
   data() {
     return {
       hello: 'Hello Vite!'
-    }
 
+    }
+  },
+  components: {
+    appTitle
   }
 }
 </script>
 
 <template>
+  <appTitle></appTitle>
   <h1>{{ hello }}</h1>
 </template>
 
